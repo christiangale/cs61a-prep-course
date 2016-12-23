fff626c0fcb5122d2801f3b15f2c07588034a2ff
christian <christian@christians-MacBook-Pro.local>
2016-12-23 11:37:31 -0800

    finsihed chapter 5

---
+++ cs61a-prep-course/Chapter 2 - 5 /first_week.rb
@@ -0,0 +1,23 @@
+#section 2
+puts 365*24
+puts (10*((365*24)*60))
+puts 60*60*24*365*18 +(9*60*60)
+puts 1160000000/60/60/24/365
+
+puts ' '
+
+#section 5
+puts 'Hello there, and what\'s your first name?'
+firstname = gets.chomp
+puts 'What\'s your middle name?'
+middlename = gets.chomp
+puts 'What about your last name?'
+lastname = gets.chomp
+puts 'Well hello ' + firstname.to_s + ' ' + middlename + ' ' + lastname.to_s + '!'
+
+puts ''
+
+puts 'Whats your favorite number?'
+number = gets.chomp
+number = number.to_i + 1
+puts 'This is a bigger and better number: ' + number.to_s
