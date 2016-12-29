ef470ff9ea549ad03c51deaabca2e3ff70c036a8
christian <christian@christians-MacBook-Pro.local>
2016-12-28 16:20:55 -0800

    Chapter 6 exercises

---
+++ ch6/angryBoss.rb
@@ -0,0 +1,5 @@
+puts 'What do you want!?!?'
+
+answer = gets.chomp
+
+puts answer.to_s.upcase + '! YOU\'RE FIRED!'
---
+++ ch6/tableOfContents.rb
@@ -0,0 +1,6 @@
+line = 40
+puts('Table of Contents'.center(line))
+puts(''.center(line))
+puts('Chapter 1:  Getting Started'.ljust(line/2) + 'page  1'.rjust(26/2))
+puts('Chapter 2:  Numbers'.ljust(line/2) + 'page  9'.rjust(line/2))
+puts('Chapter 3:  Letters'.ljust(line/2) + 'page 13'.rjust(line/2))
